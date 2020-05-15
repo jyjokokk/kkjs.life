e715986e140297de30ccf6ab3b8dfea9600c6582
jyjokokk <jyrki.j.kokkola@student.jyu.fi>
2020-05-15 20:01:17 +0300

    Nyt toimii.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Kortepohjan Kristillinen Juomaseura 4 LIFE");
+// Barebones NodeJS server that serves HTML
+var express = require('express');
+var app = express();
+var path = require('path');
+// const PORT = process.env.PORT || 5000
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(8080);
