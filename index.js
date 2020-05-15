322c8216cd154e1405e30515a7974cb59cbac066
jyjokokk <jyrki.j.kokkola@student.jyu.fi>
2020-05-15 20:17:34 +0300

    Ehka

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 // Barebones NodeJS server that serves HTML
-var express = require('express');
-var app = express();
-var path = require('path');
-var PORT = process.env.PORT || 5000
-// const PORT = process.env.PORT || 5000
+const express = require('express');
+const app = express()
+const PORT = process.env.PORT || 5000
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
+app.get('/', (req, res) => {
+  res.send('<h1>KORTEPOHJAN KRISTILLISEN JUOMASEURA FOR LIFE</h1>')
 });
 
-app.listen(8080);
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`)
+});
+
