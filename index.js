bc59ab4fef257527deb8971c6d10a06e56a9ae19
jyjokokk <jyrki.j.kokkola@student.jyu.fi>
2020-05-16 06:38:23 +0300

    Serve HTML as file

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 // Barebones NodeJS server that serves HTML
 const express = require('express');
-const app = express()
+const path = require('path')
 const PORT = process.env.PORT || 5000
+const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>KORTEPOHJAN KRISTILLISEN JUOMASEURA FOR LIFE</h1>')
+  res.sendFile(path.join(__dirname + '/public/index.html'));
 });
+
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`)
